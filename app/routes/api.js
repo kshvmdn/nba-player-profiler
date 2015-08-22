0655b15784535168f9dd0f793acf9ff1ba93a5e2
Kashav Madan <madanballer@hotmail.com>
2015-08-22 04:40:39 -0400

    Added scraping for avgs per game (all time)

--- app/routes/api.js
+++ app/routes/api.js
@@ -6,18 +6,37 @@ var router = express.Router();
 
 router.get("/:url", function(req,res) {
 	//var url = req.params.url;
+
 	var url = "http://www.basketball-reference.com/players/r/rosede01.html";
 
 	request(url, function(error, response, body) {
 		if (error) throw error;
 		var $ = cheerio.load(body);
 
-		var player_info = {name: "", img_src: ""}
+		var player = {info: {}, stats: {}}
+
+		// player info
 		$("#info_box").filter(function() {
-			player_info.name = $(this).find('h1').text();
-			player_info.img_src = $(this).find('img').attr('src');
+			player.info.name = $(this).find('h1').text();
+			player.info.img_src = $(this).find('img').attr('src');
+			// TODO get more info lol
 		});
-		res.send(player_info);
+
+		// player stats
+		$('#per_game').filter(function() {
+			var stats = {};
+			var temp = [];
+			$(this).find('tfoot td').each(function(i, el) {
+				if (i > 4)
+					temp.push(parseFloat($(this).text()));
+			});
+			$(this).find('thead th').each(function(i, el) {
+				if (i > 4)
+					stats[$(this).attr('data-stat')] = temp[i-5];
+			});
+			player.stats.per_game = stats;
+		});
+		res.send(player);
 	});
 });
 
