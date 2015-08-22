2b25d7cbf969bc5535a58bb195aad9565e6e4d45
Kashav Madan <madanballer@hotmail.com>
2015-08-22 05:00:15 -0400

    Bundled scraping for tables into single function, added all time & per-36 stats

--- app/routes/api.js
+++ app/routes/api.js
@@ -15,27 +15,50 @@ router.get("/:url", function(req,res) {
 
 		var player = {info: {}, stats: {}}
 
-		// player info
+		// INFO
 		$("#info_box").filter(function() {
 			player.info.name = $(this).find('h1').text();
 			player.info.img_src = $(this).find('img').attr('src');
 			// TODO get more info lol
 		});
 
-		// player stats
-		$('#per_game').filter(function() {
-			var stats = {};
-			var temp = [];
-			$(this).find('tfoot td').each(function(i, el) {
+		$('#contract').filter(function() {
+			
+		})
+		
+		// STATS
+		var scrape_table_values = function(tableAttr, cb) {
+			$(tableAttr).filter(function(){
+				var stats = {};
+				var temp = [];
+				$(this).find('tfoot td').each(function(i, el) {
 				if (i > 4)
 					temp.push(parseFloat($(this).text()));
+				});
+				$(this).find('thead th').each(function(i, el) {
+					if (i > 4)
+						stats[$(this).attr('data-stat')] = temp[i-5];
+				});
+				cb(null, stats);
 			});
-			$(this).find('thead th').each(function(i, el) {
-				if (i > 4)
-					stats[$(this).attr('data-stat')] = temp[i-5];
-			});
-			player.stats.per_game = stats;
+		}
+
+		// all time
+		scrape_table_values('#totals', function(err, res) {
+			player.stats.total = res;
 		});
+
+		// per game
+		scrape_table_values('#per_game', function(err, res) {
+			player.stats.per_game = res;
+		});
+
+		// per 36 min
+		scrape_table_values('#per_minute', function(err, res) {
+			player.stats.per_36_min = res;
+		})
+
+		
 		res.send(player);
 	});
 });
