98b057c3edd8c9ab808794106173cdb9ca6ae2d4
Kashav Madan <madanballer@hotmail.com>
2015-08-22 03:15:04 -0400

    Scrapes player name and img src

--- app/routes/api.js
+++ app/routes/api.js
@@ -7,7 +7,18 @@ var router = express.Router();
 router.get("/:url", function(req,res) {
 	//var url = req.params.url;
 	var url = "http://www.basketball-reference.com/players/r/rosede01.html";
-	res.json({title:"Derrick Rose", info: url});
+
+	request(url, function(error, response, body) {
+		if (error) throw error;
+		var $ = cheerio.load(body);
+
+		var player_info = {name: "", img_src: ""}
+		$("#info_box").filter(function() {
+			player_info.name = $(this).find('h1').text();
+			player_info.img_src = $(this).find('img').attr('src');
+		});
+		res.send(player_info);
+	});
 });
 
 module.exports = router;
