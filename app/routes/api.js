7839d310161e0cd63dd16e6fb219a1896746c50a
Kashav Madan <madanballer@hotmail.com>
2015-08-22 14:35:40 -0400

    Gets player info given name rather than BBR link

--- app/routes/api.js
+++ app/routes/api.js
@@ -4,63 +4,97 @@ var cheerio = require("cheerio");
 
 var router = express.Router();
 
-router.get("/:url", function(req,res) {
-	//var url = req.params.url;
+router.post("/player", function(req,res) {
+	var name = req.body.player_name.trim();
+	var nameArr = name.split(" ");
+	
+	// first letter of last name;
+	var l = nameArr[1][0];
 
-	var url = "http://www.basketball-reference.com/players/r/rosede01.html";
+	var host = "http://www.basketball-reference.com";
+	var playerList = host + "/players/" + l + "/";
 
-	request(url, function(error, response, body) {
-		if (error) throw error;
-		var $ = cheerio.load(body);
+	var getPlayerPath = function(callback) {
+		request(playerList, function(error, response, body) {
+			if (error) throw error;
+			var $ = cheerio.load(body);
+			var playerPath = $('#players tbody tr').filter(function() {
+				return $(this).find('td').first().find('a').text().toLowerCase() === name.toLowerCase();
+			}).find('a').attr('href');
 
-		var player = {info: {}, stats: {}}
-
-		// INFO
-		$("#info_box").filter(function() {
-			player.info.name = $(this).find('h1').text();
-			player.info.img_src = $(this).find('img').attr('src');
-			// TODO get more info lol
+			callback(null, playerPath);
 		});
+	}
 
-		$('#contract').filter(function() {
-			
-		})
-		
-		// STATS
-		var scrape_table_values = function(tableAttr, cb) {
-			$(tableAttr).filter(function(){
-				var stats = {};
-				var temp = [];
-				$(this).find('tfoot td').each(function(i, el) {
-				if (i > 4)
-					temp.push(parseFloat($(this).text()));
-				});
-				$(this).find('thead th').each(function(i, el) {
-					if (i > 4)
-						stats[$(this).attr('data-stat')] = temp[i-5];
-				});
-				cb(null, stats);
+	var getPlayerInfo = function(playerUrl, callback) {
+		var url = playerUrl;
+		request(url, function(error, response, body) {
+			if (error) throw error;
+			var $ = cheerio.load(body);
+
+			var player = {info: {}, stats: {}}
+
+			player.info.bbr_link = url;
+
+			// INFO
+			$("#info_box").filter(function() {
+				player.info.name = $(this).find('h1').text();
+				player.info.img_src = $(this).find('img').attr('src');
+				// TODO get more info lol
 			});
-		}
 
-		// all time
-		scrape_table_values('#totals', function(err, res) {
-			player.stats.total = res;
+			$('#contract').filter(function() {
+				// if table#contract exists, player's still active
+				player.info.current_contract = $(this).find('tr').last().find('td').eq(1).text()
+			});
+			
+			// STATS
+			var scrapeTable = function(tableAttr, cb) {
+				$(tableAttr).filter(function(){
+					var stats = {};
+					var temp = [];
+					$(this).find('tfoot td').each(function(i, el) {
+					if (i > 4)
+						temp.push(parseFloat($(this).text()));
+					});
+					$(this).find('thead th').each(function(i, el) {
+						if (i > 4)
+							stats[$(this).attr('data-stat')] = temp[i-5];
+					});
+					cb(null, stats);
+				});
+			}
+
+			// all time
+			scrapeTable('#totals', function(err, res) {
+				player.stats.total = res;
+			});
+
+			// per game
+			scrapeTable('#per_game', function(err, res) {
+				player.stats.per_game = res;
+			});
+
+			// per 36 minutes
+			scrapeTable('#per_minute', function(err, res) {
+				player.stats.per_36_min = res;
+			});
+			
+			// per 100 possessions
+			scrapeTable('#per_poss', function(err, res) {
+				player.stats.per_100_poss = res;
+			});
+
+			callback(null, player);
 		});
+	}
 
-		// per game
-		scrape_table_values('#per_game', function(err, res) {
-			player.stats.per_game = res;
+	getPlayerPath(function(err, path) {
+		var url = host + path;
+		getPlayerInfo(url, function(err, info) {
+			res.json(info);
 		});
-
-		// per 36 min
-		scrape_table_values('#per_minute', function(err, res) {
-			player.stats.per_36_min = res;
-		})
-
-		
-		res.send(player);
 	});
-});
+})
 
 module.exports = router;
